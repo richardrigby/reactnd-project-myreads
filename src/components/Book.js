098d03c2f77df7885937c13ee5c339115306c990
Richard Rigby <rrigby@redzone.com>
2017-09-23 11:48:40 +1200

    refactor: Create 'Book' component and refactor to use this component
    This component now replases all the repeated inline 'Book' code within 'App.js'
    This will form the starting point to reactify the app.

---
+++ src/components/Book.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+class Book extends React.Component {
+  render() {
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: this.props.width, height: this.props.height, backgroundImage: this.props.backgroundImage }}></div>
+          <div className="book-shelf-changer">
+            <select>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{this.props.bookTitle}</div>
+        <div className="book-authors">{this.props.bookAuthors}</div>
+      </div>
+    );
+  }
+};
+
+export default Book;
+
+
